1e8d7f42f6ca59addb1eafa019f16358b43452e8
Juan David Franco <jfrancoh5@gmail.com>
2024-03-08 23:17:26 -0500

    boton de hamburguesa

---
+++ styles.js
@@ -0,0 +1,10 @@
+const secondButtonBurguertLine = document.getElementById('button-second-line');
+const thirdButtonBurguerLine = document.getElementById('button-third-line');
+const buttonBurguer = document.getElementById('button-burguer');
+
+function modifySecondLine(){
+    secondButtonBurguertLine.classList.toggle('button-second-line-change');
+    thirdButtonBurguerLine.classList.toggle('button-third-line-change');
+}
+
+buttonBurguer.addEventListener('click',modifySecondLine);
