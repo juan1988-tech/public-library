cf455aada77db332ca0dfbe14cd80925fdb1dc30
Juan David Franco <jfrancoh5@gmail.com>
2024-03-09 00:53:29 -0500

    burguer menu header finished

--- styles.js
+++ styles.js
@@ -1,10 +1,12 @@
 const secondButtonBurguertLine = document.getElementById('button-second-line');
 const thirdButtonBurguerLine = document.getElementById('button-third-line');
 const buttonBurguer = document.getElementById('button-burguer');
+const navbarMenu = document.querySelector('#navbar-menu');
 
 function modifySecondLine(){
     secondButtonBurguertLine.classList.toggle('button-second-line-change');
     thirdButtonBurguerLine.classList.toggle('button-third-line-change');
+    navbarMenu.classList.toggle('navbar-menu-movement');
 }
 
 buttonBurguer.addEventListener('click',modifySecondLine);
