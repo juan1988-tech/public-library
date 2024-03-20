6fb887e1dfb4f4a10f2ef6dd411add087fcfd31c
Juan David Franco <jfrancoh5@gmail.com>
2024-03-20 17:36:30 -0500

    funcion de cambio de estado del libro en el constructor

--- books.js
+++ books.js
@@ -1,27 +1,3 @@
-// primer objeto
-
-/* let book = {
-    id: 2,
-    genre: 'oconomía',
-    author: 'Joseph Stitfitz',
-    year: 2018,
-    editorial: 'Sanlana Editores',
-    typeBook: 'libro de economia',
-    state:{
-        available: true,
-        loaned: false    
-    },
-    //funciones con respecto a los nodos
-    //cathalog-main
-    cathalogMainNode: document.querySelector('.cathalog-main'),
-    
-    //cathalog-main-book al final irá al nodo al cual se añade
-    cathalogMainBookNode: document.createElement('section'),
-    cathalogMainBookClass: function(){
-        this.cathalogMainBookNode.classList = "cathalog-main-book";
-    } 
-} */
-
 class MainBook{
     constructor(id,nameBook,genre,author,year,editorial,typeBook,state,stateBook){
         this.id = id;
@@ -32,10 +8,8 @@ class MainBook{
         this.editorial = editorial;
         this.typeBook = typeBook;
         this.state = state;
-        this.stateBook = stateBook;     
-    }
-    //funcion para si el libro está en préstamo o disponible
-    changeStateBook(){
+        this.stateBook = stateBook; 
+        
         if(this.state === true){
             this.stateBook = 'Disponible';
             return  this.stateBook;
@@ -43,16 +17,17 @@ class MainBook{
         else{
             this.stateBook = "En préstamo";
             return this.stateBook;
-        }
+         } 
     }
-    
+        
 }
+  
 
 const firstBook = new MainBook(1,'Caída Libre','Economía','Joseph Stiglitz',2008,
-'Santillana Ediciones Generales','libro de tapa dura',true,'');
+'Santillana Ediciones Generales','libro de tapa dura',true);
+console.log(firstBook);
 
-const secondBook = new MainBook(2,'Inteligencia Emocional','Psicoogía','Daniel Coleman',2018,
-'Penguin Ranom Editorial','texto de divulgacion científica',false,'')
+
 
 
 
